ab74cec90580b10e7242c780f7ed3df3dcc782e7
Radoslaw Roszczyk <rroszczyk@users.noreply.github.com>
2022-04-30 11:10:31 +0200

    Create fiby.py

---
+++ fiby.py
@@ -0,0 +1,12 @@
+def power(a, b):
+  print(a ** b)
+
+def fib(n):
+  a, b = 0, 1
+  while a < n:
+    print(a, end=' ')
+    a, b = b, a + b
+  print()
+
+if __name__ == "__main__":
+  print("To nie jest moduł wykonywalny - uruchom ten kod z głównego programu")
